63250bea480d7fbce3a4832843a1b143f95c9394
Johnatan <johrasamoelina@gmail.com>
2025-07-17 11:10:54 +0300

    modified urls.py

--- plantapi/plantapi/urls.py
+++ plantapi/plantapi/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/scans/', include('scans.urls')),  # <- ici ny historique
+    path('api/scans/', include('scans.urls')),# <- ici ny historique
+    path('api/accounts/', include('scans.urls')) , # <- ici les comptes utilisateurs
 ]
